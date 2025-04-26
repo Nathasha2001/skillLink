9dfc2d6ae7dd205ec4d55aa5a760554327aafef4
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2025-04-26 12:43:21 +0530

    update

--- levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
+++ levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000") local host make
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true)
