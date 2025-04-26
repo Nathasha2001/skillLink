0ad016704ebab78b1055e5ee16c07ecb61ab3bcc
Savindi <155376122+Shadow11t@users.noreply.github.com>
2025-04-26 17:38:03 +0530

    updated

--- levelup/backend/src/main/java/com/levelup/backend/config/JwtAuthenticationFilter.java
+++ levelup/backend/src/main/java/com/levelup/backend/config/JwtAuthenticationFilter.java
@@ -46,7 +46,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     authenticationToken.setDetails(
                             new WebAuthenticationDetailsSource().buildDetails(request)
                     );
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);//set authentication
                 }
             }
         }
