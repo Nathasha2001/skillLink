966fc52979c29785df2428fb1a0925eaaa631d82
AmilaIsuru <152780113+AmilaIsuru@users.noreply.github.com>
2025-05-07 21:14:49 +0530

    updated

--- levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
+++ levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
+                        .allowedMethods("*") 
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .exposedHeaders("Authorization");
