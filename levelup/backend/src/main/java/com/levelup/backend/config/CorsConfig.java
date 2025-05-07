5904738488ceb5ad859a7809cbbdf0e7d368221b
AmilaIsuru <152780113+AmilaIsuru@users.noreply.github.com>
2025-05-07 21:15:58 +0530

    updated v2

--- levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
+++ levelup/backend/src/main/java/com/levelup/backend/config/CorsConfig.java
@@ -9,10 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer() { //funtion
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) { 
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("*") 
