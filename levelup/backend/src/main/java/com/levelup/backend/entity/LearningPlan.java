c2b531a8b81c61562f14ddead1b71e2afe9cff2b
Nathasha <142737136+Nathasha2001@users.noreply.github.com>
2025-05-10 19:07:15 +0530

    up

--- levelup/backend/src/main/java/com/levelup/backend/entity/LearningPlan.java
+++ levelup/backend/src/main/java/com/levelup/backend/entity/LearningPlan.java
@@ -6,7 +6,15 @@ import java.util.List;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "learning_plans")
